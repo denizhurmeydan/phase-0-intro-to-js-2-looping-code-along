98ccdd43b3def8a540b794de279c4273ac6c280b
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-05-05 17:44:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const cards = writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+function writeCards(names) {
+    const cards = names.map((name) => `Thank you, ${name}, for the wonderful surprise gift!`
+    );
+    return cards;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  function countDownLog(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(`Counting down: ${i}`);
+    }
+  }
